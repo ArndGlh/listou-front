9f7b73aeeef9a3c564704ba97375e5ef5298d145
Arnaud Guilhaumon <arnaud.guilhaumon@2si.local>
2021-02-16 17:48:13 +0100

    post avatar et get, not working

--- src/app/_services/file.service.ts
+++ src/app/_services/file.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, Subject } from 'rxjs';
+import { Observable, of, Subject } from 'rxjs';
 import { FileInput } from 'ngx-material-file-input';
+import { TokenStorageService } from './token-storage.service';
 
 const API_URL = 'http://localhost:8080/avatar/';
 
@@ -10,16 +11,20 @@ const API_URL = 'http://localhost:8080/avatar/';
 })
 export class FileService {
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient,
+    private tokenStorageService: TokenStorageService) {
   }
 
   public updateAvatar(fileInput: FileInput): Observable<any>  {
+    const user = this.tokenStorageService.getUser();
     const file_form: FileInput = fileInput;
     const file = file_form.files[0];
 
     const formData = new FormData();
     formData.append('file', file);
+    formData.append('userId', user.id);
 
     return this.http.post(API_URL + 'upload', formData);
+
   }
 }
--- src/app/components/user/profile/profile-details/profile-details.component.ts
+++ src/app/components/user/profile/profile-details/profile-details.component.ts
@@ -1,8 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { FileInput, FileValidator } from 'ngx-material-file-input';
 import { ToastrService } from 'ngx-toastr';
 import { FileService } from 'src/app/_services/file.service';
+import { TokenStorageService } from 'src/app/_services/token-storage.service';
 
 @Component({
   selector: 'profile-details',
@@ -14,22 +16,41 @@ export class ProfileDetailsComponent implements OnInit {
   public form: FormGroup;
   readonly maxSize = 200000;
 
+  public avatarUrl: SafeUrl;
+
   constructor(private _fb: FormBuilder,
     private fileService: FileService,
-    private toastr: ToastrService) {
+    private toastr: ToastrService,
+    private tokenStorageService: TokenStorageService,
+    private sanitizer : DomSanitizer) {
     this.form = this._fb.group({
       avatarFile: [
         undefined,
         [FileValidator.maxContentSize(this.maxSize)]
       ]
     });
+    this.avatarUrl = '';
    }
 
   ngOnInit(): void {
+
+    const user = this.tokenStorageService.getUser();
+    if(user.avatar != null){
+      let reader = new FileReader();
+      reader.readAsDataURL(user.avatar);
+      this.avatarUrl = this.sanitizer.bypassSecurityTrustUrl('data:image/png;base64,' + reader.result);
+    } else {
+      this.avatarUrl = '//ssl.gstatic.com/accounts/ui/avatar_2x.png';
+    }
+
+
+
+
+
   }
 
   public onSubmit(){
-    this.fileService.updateAvatar(this.form.get('avatarFile')!.value).subscribe(
+    this.fileService.updateAvatar(this.form.get('avatarFile')!.value).subscribe( // TODO check value avatarfile null
       data => {
         console.log(data);
         this.toastr.success('Mise a jour réussie !');
